1d1d5b13f4435863af2dce2ab71d5e7d78871af2
Edgard Arakaki <edgard.arakaki@gmail.com>
2012-01-08 16:59:46 -0200

    Fixed #1: Outdated yasnippet url

--- emacs-snippet.el
+++ emacs-snippet.el
@@ -1,4 +1,20 @@
-(add-to-list 'install 'yasnippet)
+(add-to-list 'custom-install
+             '(:name yasnippet
+                     :website "https://github.com/capitaomorte/yasnippet.git"
+                     :description "YASnippet is a template system for Emacs."
+                     :type git
+                     :url "https://github.com/capitaomorte/yasnippet.git"
+                     :features "yasnippet"
+                     :prepare (lambda ()
+                                (unless (or (boundp 'yas/snippet-dirs) (get 'yas/snippet-dirs 'customized-value))
+                                  (setq yas/snippet-dirs
+                                        (list (concat el-get-dir (file-name-as-directory "yasnippet") "snippets")))))
+
+                     :post-init (lambda ()
+                                  (put 'yas/snippet-dirs 'standard-value
+                                       (list (list 'quote
+                                                   (list (concat el-get-dir (file-name-as-directory "yasnippet") "snippets"))))))
+                     :compile nil))
 
 (eval-after-load "yasnippet"
   '(progn
